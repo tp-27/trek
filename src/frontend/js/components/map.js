84e0180df1f5a4782bbe534a4872a7e769e9d3f8
Thomas Phan <thomasphan@Thomass-Air.lan>
2023-12-16 10:25:04 -0500

    init map

--- src/frontend/js/components/map.js
+++ src/frontend/js/components/map.js
@@ -1,10 +1,13 @@
 var map; 
 
 function initMap(park) {
-    removeChooseParkBtn();
+    var mapDiv = document.getElementById("map");
+
+    mapDiv.style.height = "600px";
+    mapDiv.style.width = "90%";
+    removeChooseParkBtn(); // remove select park button 
 
     L.map('map'); // create a new map, associate it with element id 'map'
-
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', { // initialize map with tile layer 
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
@@ -12,7 +15,6 @@ function initMap(park) {
 
     map.setView([51.505, -0.09], 13); // set map view to specified coordinates and zoom level
 }
-
 
 function removeChooseParkBtn() {
     var parkBtn = document.getElementById("park-btn");
