bd002fa8e96bd6484fa74799b1bf228a4753c9a2
Thomas Phan <thomasphan@Thomass-Air.lan>
2023-12-16 10:21:13 -0500

    created choose a park button

--- src/frontend/js/components/map.js
+++ src/frontend/js/components/map.js
@@ -1,10 +1,20 @@
-var map = L.map('map'); // create a new map, associate it with element id 'map'
+var map; 
 
-function initMap(park, map) {
+function initMap(park) {
+    removeChooseParkBtn();
+
+    L.map('map'); // create a new map, associate it with element id 'map'
+
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', { // initialize map with tile layer 
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
 
     map.setView([51.505, -0.09], 13); // set map view to specified coordinates and zoom level
+}
+
+
+function removeChooseParkBtn() {
+    var parkBtn = document.getElementById("park-btn");
+    parkBtn.style.display = "none";
 }
