630fa4dbf34ab9d01002ab4ca6067983e3252d28
0graph <50416380+0graph@users.noreply.github.com>
2023-12-16 11:41:29 -0500

    Update map.js
    fixed map init

--- src/frontend/js/components/map.js
+++ src/frontend/js/components/map.js
@@ -7,14 +7,17 @@ function initMap(park) {
     mapDiv.style.width = "90%";
     removeChooseParkBtn(); // remove select park button 
 
-    L.map('map'); // create a new map, associate it with element id 'map'
+    var map = L.map("map", {
+        center: [-1.2836622060674874, 36.822524070739746],
+        zoom: 16 //set the zoom level
+    });
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', { // initialize map with tile layer 
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
 
     map.setView([51.505, -0.09], 13); // set map view to specified coordinates and zoom level
-}
+}   
 
 function removeChooseParkBtn() {
     var parkBtn = document.getElementById("park-btn");
