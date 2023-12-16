d5c57a09ed603a004bb06a9952c323e5fe617af4
0graph <50416380+0graph@users.noreply.github.com>
2023-12-16 15:57:22 -0500

    Update map.js
    got addlayer working

--- src/frontend/js/components/map.js
+++ src/frontend/js/components/map.js
@@ -40,15 +40,19 @@ function removeChooseParkBtn() {
 
 var baseURL = "http://52.15.34.182:8080/geoserver/wfs?service=wfs&version=2.0.0&request=getfeature&typename="; //Geographic Web File Service
 
-function getLayer(layerName) {
+async function getLayer(layerName) {
     fetch(baseURL + layerName + "&outputFormat=application/json")
     .then(response => {
         if(!response.ok) {
             throw new Error('Error: Invalid Response');
         }
-
+        console.log(response);
         return response.json();
     })
+    .then(data => {
+        console.log(data);
+        L.geoJSON(data).addTo(mapLayerGroup);
+      })
     .catch(error => {
         console.error('Error:', error);
     });
