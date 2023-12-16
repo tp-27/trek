8dfac5f61ac88c581e8f56518ffd5050b302cc70
Griffin <griffin.r.baird@gmail.com>
2023-12-16 16:35:17 -0500

    canoe icons

--- src/frontend/js/components/map.js
+++ src/frontend/js/components/map.js
@@ -29,24 +29,43 @@ function initMap(park) {
 
 
 function markers(map){
+    var canoe_icon = L.icon({
+        iconUrl: "../../src/frontend/assets/Start_canoe.png",
+        //shadowUrl: "../../src/frontend/assets/leaf-shadow.png",
+    
+        iconSize:     [38, 95], // size of the icon
+       // shadowSize:   [50, 64], // size of the shadow
+        iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+       // shadowAnchor: [4, 62],  // the same for the shadow
+        popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+    });
+
+  
     var start = L.marker((srt_view), {draggable: true,
-        autoPan: true}).addTo(map);
+        autoPan: true, icon : canoe_icon}).addTo(map);
+
         var end = L.marker([45.84, -78.10], {draggable: true,
-            autoPan: true}).addTo(map);
+            autoPan: true, icon: canoe_icon}).addTo(map);
         
-        start.bindPopup("Start.");
-        end.bindPopup("End.");
+        start.bindPopup("Start" +  start.getLatLng());
+        end.bindPopup("End." + end.getLatLng());
     
             start.on('dragend', function(event) {
                 var S_latlng = event.target.getLatLng();
                 console.log("START: ", S_latlng.lat, S_latlng.lng)
+                start.bindPopup("Start" +  start.getLatLng());
               });
     
     
             end.on('dragend', function(event) {
                 var E_latlng = event.target.getLatLng();
                 console.log("END: ", E_latlng.lat, E_latlng.lng)
+                end.bindPopup("End." + end.getLatLng());
               });
+
+
+
+              
 }
 
 
