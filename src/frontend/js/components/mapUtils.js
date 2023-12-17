241dd24d5f384a0b4cf313f3758c35f9afe8935a
0graph <50416380+0graph@users.noreply.github.com>
2023-12-17 12:25:01 -0500

    Update mapUtils.js
    added getpath and addpath

--- src/frontend/js/components/mapUtils.js
+++ src/frontend/js/components/mapUtils.js
@@ -1,13 +1,21 @@
 const baseURL = "http://52.15.34.182:8080/geoserver/wfs?service=wfs&version=2.0.0&request=getfeature&typename="; //Geographic Web File Service
+const respFormat = "&outputFormat=application/json";
+var sourceID = 0;
+var targetID = 0;
 
-export async function addLayer (layerName, mapLayerGroup) {
+export async function addLayer(layerName, mapLayerGroup) {
     getLayer(layerName)
         .then(data =>  L.geoJSON(data).addTo(mapLayerGroup)) // add layer to layer group
         .catch(err => console.log("Rejected: " + err.message));
 }
 
+export async function addPath(mapLayerGroup) {
+    getPath()
+    .then(data =>  L.geoJSON(data).addTo(mapLayerGroup)) // add layer to layer group
+    .catch(err => console.log("Rejected: " + err.message));
+}
+
 async function getLayer(layerName) {
-    var respFormat = "&outputFormat=application/json";
 
     const response = await fetch(baseURL + layerName + respFormat);
     const geoJSON = await response.json();
@@ -16,7 +24,13 @@ async function getLayer(layerName) {
 }
 
 async function getNearestVertex(point) {
-    var url = `${baseURL}nearest_vertex&outputformat=application/json&viewparams=x:${point.lng};y:${point.lat};`;
+    var url = `${baseURL}nearest_vertex${respFormat}&viewparams=x:${point.lng};y:${point.lat};`;
+    const response = await fetch(url);
+    return response.json();
+}
+
+async function getPath() {
+    var url = `${baseURL}shortest_path${respFormat}&viewparams=source:${sourceID};target:${targetID};`
     const response = await fetch(url);
     return response.json();
 }
