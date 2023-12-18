8d3d29eda48b438647c214078067d97e6d585c98
0graph <50416380+0graph@users.noreply.github.com>
2023-12-18 11:55:17 -0500

    added template for mapsettings in main

--- src/frontend/js/components/mapSettings.js
+++ src/frontend/js/components/mapSettings.js
@@ -1,11 +1,16 @@
-class mapSettings {
+export class mapSettings {
     constructor() {
         //Set defaults
         //this.{setting} = ...
 
         //Cost Settings
-
+        this.triplePortageDistance = false;
         //Speed Settings
+        this.canoeSpeed = 4; //Km/h
+        this.portageSpeed = 2; //Km/h
+        //Route Settings
+        this.maxDifficultLevel = 3; //1 = easy only, 2 = easy, intermediate, 3 = easy, intermediate, hard
+
 
         //Tile Setgins
 
--- src/frontend/js/main.js
+++ src/frontend/js/main.js
@@ -1,4 +1,8 @@
 import { initMap } from './components/map.js';
+import { mapSettings, mapSettings } from './components/mapSettings.js';
+
+
+var mapSettings = new mapSettings();
 
 // scroll animations
 const observer = new IntersectionObserver((entries) => {
@@ -17,4 +21,11 @@ window.addEventListener('DOMContentLoaded', function() {
     document.getElementById('park-btn').addEventListener("click", () => {
         initMap();
     });
+
+    //Settings Functions
+    //document.getElementById('{SettingName}').addEventListener("click", () => {
+    //      // Get Value
+    //      mapSettings.set{SettingName}();
+    //      mapSettings.setLocalStorage({SettingName},{Value})
+    //});
 });
